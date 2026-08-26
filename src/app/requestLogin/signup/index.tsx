c7bbc77ffe6dc28a1154c9a14a5872a5e1b837a9
Camellia <thehead4ever2@gmail.com>
2026-08-26 11:58:37 +0330

    now the sign up works, needs some cleanup

--- src/app/requestLogin/signup/index.tsx
+++ src/app/requestLogin/signup/index.tsx
@@ -2,15 +2,22 @@ import Button from "@/src/components/Button/Button";
 import axios from "axios";
 import { router } from "expo-router";
 import React, { useState } from "react";
-import { ColorValue, Text, TextInput, View } from "react-native";
+import { ColorValue, Platform, Text, TextInput, View } from "react-native";
 import { useCSSVariable } from "uniwind";
 export default function index() {
   const [emailValue, setEmailValue] = useState<string>("");
   const [passwordValue, setPasswordValue] = useState<string>("");
   const [nameValue, setNameValue] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
   const placeholderColor = useCSSVariable("--color-primary");
   const inputClassNames =
     "bg-card border-1 border-border text-2xl px-4 rounded-3xl text-foreground ";
+
+  const baseAddress =
+    Platform.OS == "android"
+      ? "http://10.0.2.2:3000/"
+      : "http://localhost:3000/";
+
   return (
     <View className="flex-1 bg-background">
       <Text className="text-center text-3xl my-6 text-foreground ">
@@ -45,12 +52,18 @@ export default function index() {
       </View>
       <View className="flex-row justify-center my-8 gap-6">
         <Button
-          onPress={() => {
-            axios.post("localhost:3000/auth/signup", {
+          onPress={async () => {
+            const response = await axios.post(`${baseAddress}auth/signup`, {
               name: nameValue,
               email: emailValue,
               password: passwordValue,
             });
+            setResponse(response.data.email);
+            setEmailValue("");
+            setPasswordValue("");
+            setNameValue("");
+
+            console.log(response);
           }}
           className="px-4 rounded-xl"
         >
@@ -65,6 +78,7 @@ export default function index() {
           <Text className="text-2xl text-foreground text-accent">cancel</Text>
         </Button>
       </View>
+      <Text className="text-xl text-primary text-center">{response}</Text>
     </View>
   );
 }
